ceafff81a2dd9255347ca7e9f0abba0f2c38c6f1
AwangDarmawan <adarmawan468@gmail.com>
2026-07-20 20:42:57 +0700

    report binar

--- src/Components/Ketiga/DataCertificate.jsx
+++ src/Components/Ketiga/DataCertificate.jsx
@@ -2,6 +2,7 @@ import Binar from "../../assets/Img/Binar.png";
 import IPL from "../../assets/Img/IPL.jpg";
 import Godot from "../../assets/Img/Godot.jpg";
 import Report1 from "../../assets/Img/ReportBinar1.jpg";
+import Report2 from "../../assets/Img/ReportBinar2.jpg";
 
 
 const DataCertificate = [
@@ -18,16 +19,22 @@ const DataCertificate = [
     year : "1 Tahun",
   },
    {
-    title: "Game Godot 3D",
+    title: "Membangun Game Godot 3D",
     issuer: "TC Creation",
     image: Godot,
     year: "4 Bulan"
   },
   {
-    title: "Student Report Frontend",
+    title: "Magang Report Frontend",
     issuer: "Binar Academy  -Predikat Pujian ",
     image: Report1,
     year : "5 Bulan"
+  },
+    {
+    title: "Magang report Frontend ",
+    issuer: "Binar Academy -Predikat Pujian ",
+    image: Report2,
+      year : "5 Bulan",
   },
  
 ];
